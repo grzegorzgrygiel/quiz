d80e4a48da308ce710c11c16a7458acb6fffaa73
ggregi <grygregi@gmail.com>
2023-02-23 19:03:20 +0100

    Final changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const settingsDiv = document.querySelector("#settings");
 const number = document.querySelector("#number");
 const topic = document.querySelector("#topic");
 const level = document.querySelector("#level");
@@ -6,6 +7,8 @@ const newQuiz = document.querySelector("#newQuiz");
 const nextQuestion = document.querySelector("#newQuestion");
 const questionContainer = document.querySelector("#quizContainer");
 const cancel = document.querySelector("#renew");
+const showScore = document.querySelector("#score");
+const info = document.querySelector("#info");
 
 let id = 0;
 let questions = [];
@@ -24,6 +27,8 @@ async function getData(url, params) {
   return res;
 }
 
+// %%%%%%%%% GET QUESTIONS FROM API %%%%%%%%%%%%%
+
 async function getQuestions() {
   try {
     const params = {
@@ -39,6 +44,10 @@ async function getQuestions() {
   }
 }
 
+// %%%%%%%%%% DISPLAY 1ST QUESTION %%%%%%%%%%%%%%
+
+// function for the START QUIZ button
+
 async function saveQuestions() {
   questions = await getQuestions();
   addNewQuestion(id, questions[id]);
@@ -46,11 +55,10 @@ async function saveQuestions() {
   newQuiz.disabled = true;
   renew.disabled = false;
   nextQuestion.disabled = false;
+  settingsDiv.style.display = 'none'
 }
 
-function shuffle(array) {
-  return array.sort(() => Math.random() - 0.6);
-}
+// %%%%%%%% FORMAT RADIO ANSWERS %%%%%%%%%%%%%%
 
 function renderRadio(id, answer) {
   let radiobox = document.createElement("input");
@@ -72,7 +80,14 @@ function renderRadio(id, answer) {
   questionContainer.appendChild(newline);
 }
 
+// %%%%%%%%%%% DISPLAY QUESTION & ANSWERS %%%%%%%%
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.6);
+}
+
 function addNewQuestion(id, questionData) {
+  info.style.display = 'none';
   const newQuestionTitle = document.createElement("P");
   newQuestionTitle.classList.add("question");
   newQuestionTitle.innerHTML = `Q${id + 1}. ${questionData.question}`;
@@ -97,28 +112,41 @@ function addNewQuestion(id, questionData) {
     renderRadio("B", shuffledOptions[1]);
   }
 }
+//  function for the NEXT button
 
-function addAnswer(id, data) {
-  const question = document.createElement("P");
-  question.classList.add("question");
-  const correctAnswer = document.createElement("P");
-  correctAnswer.classList.add("green");
-  const selectedAnswer = document.createElement("P");
-  selectedAnswer.classList.add("red");
-  question.innerHTML = `Q${id+1}. ${data[id].question}`;
-  correctAnswer.innerHTML = `A: ${data[id].correct}`;
-  selectedAnswer.innerHTML = `A: ${data[id].selected}`;
-  let corAns = data[id].correct;
-  let selAns = data[id].selected;
-  if (selAns === corAns) {
-    questionContainer.append(question);
-    questionContainer.append(correctAnswer);
-  } else {
-    questionContainer.append(question);
-    questionContainer.append(correctAnswer);
-    questionContainer.append(selectedAnswer);
+function addQuestion() {
+  try {
+    info.style.display = 'none';
+    if (id < questions.length) {
+      recordAnswer();
+      removeItem();
+      addNewQuestion(id, questions[id]);
+      id++;
+      showScore.disabled = false;
+    } else if (id === questions.length) {
+      // option for the last question:
+      recordAnswer();
+      removeItem();
+      const quizEnd1 = document.createElement("P");
+      quizEnd1.style.textAlign = 'center';
+      quizEnd1.innerHTML = "<b>This is the end of the quiz.</b>";
+      questionContainer.append(quizEnd1);
+      const quizEnd2 = document.createElement("P");
+      quizEnd2.style.textAlign = 'center';
+      quizEnd2.innerHTML = "<b>Click SCORE to see how well you;ve done.</b>";
+      questionContainer.append(quizEnd2);
+      showScore.disabled = false;
+      nextQuestion.disabled = true;
+    }
+  } catch (error) {
+    console.error('No answer has been selected');
+    info.style.display = 'inline';
+    info.innerHTML = '<span class="red">You need to select and answer!</span>';
   }
 }
+
+
+// %%%%%%%%%% KEEP SCORE & RECORD ANSWERS %%%%%%%%%%%%%
 
 function recordItem(id, question, correct, selected) {
   this.id = id;
@@ -142,15 +170,17 @@ function recordAnswer() {
   console.log(`The correct answer is: ${correctAnswer}`);
   console.log(`You selected: ${answerSelected}`);
   console.log(`Your score is: ${score}`);
-  console.log(answers);
 }
 
+// %%%%%%%%%%%%%%%%% RESET & REMOVE DISPLAYED QUESTIONS %%%%%%%%%%%%%%%%%%%%%
 
 function removeItem() {
   while (questionContainer.firstChild) {
     questionContainer.removeChild(questionContainer.firstChild);
   }
 }
+
+//  function for the RESET button
 
 function resetQuiz() {
   removeItem();
@@ -161,27 +191,65 @@ function resetQuiz() {
   newQuiz.disabled = false;
   renew.disabled = true;
   nextQuestion.disabled = true;
+  showScore.disabled = true;
+  settingsDiv.style.display = 'flex'
   console.clear();
 }
 
-function addQuestion() {
-  if (id < questions.length) {
-    recordAnswer();
-    removeItem();
-    addNewQuestion(id, questions[id]);
-    id++;
-  } else if (id === questions.length) {
-    recordAnswer();
-    removeItem();
-    const quizScore = document.createElement("P");
-    quizScore.innerHTML = `Your score is ${score} out of ${id}`;
-    questionContainer.append(quizScore);
-    for (let i = 0; i <= questions.length; i++) {
-      addAnswer(i, answers);
-    }
+// %%%%%%%%%%% DISPLAY SCORE AND CORRECT & SELECTED ANSWERS %%%%%%%%
+
+function addAnswer(id, data) {
+  const question = document.createElement("P");
+  question.classList.add("question");
+  const correctAnswer = document.createElement("P");
+  correctAnswer.classList.add("green");
+  const selectedAnswer = document.createElement("P");
+  selectedAnswer.classList.add("red");
+  question.innerHTML = `Q${id+1}. ${data[id].question}`;
+  correctAnswer.innerHTML = `A: ${data[id].correct}`;
+  selectedAnswer.innerHTML = `A: ${data[id].selected}`;
+  let corAns = data[id].correct;
+  let selAns = data[id].selected;
+  if (selAns === corAns) {
+    questionContainer.append(question);
+    questionContainer.append(correctAnswer);
+  } else {
+    questionContainer.append(question);
+    questionContainer.append(correctAnswer);
+    questionContainer.append(selectedAnswer);
   }
 }
+
+//  function for the SCORE button
+
+function displayScore() {
+  removeItem();
+  showScore.disabled = true;
+  try {
+    if (id < questions.length) {
+      const quizScore = document.createElement("P");
+      quizScore.style.fontSize = '28';
+      quizScore.innerHTML = `<b>Your score is <span class="red">${score}</span> out of ${id-1}</b>`;
+      questionContainer.append(quizScore);
+      for (let i = 0; i <= questions.length; i++) {
+        addAnswer(i, answers);
+      }
+    } else if (id === questions.length) {
+      const quizScore = document.createElement("P");
+      quizScore.style.fontSize = '28';
+      quizScore.innerHTML = `<b>Your score is <span class="red">${score}</span> out of ${id}</b>`;
+      questionContainer.append(quizScore);
+      for (let i = 0; i <= questions.length; i++) {
+        addAnswer(i, answers);
+      }
+    }
+  } catch (error) {
+    console.error('Ignore it');
+  }
+}
+
 
 newQuiz.addEventListener("click", saveQuestions);
 nextQuestion.addEventListener("click", addQuestion);
 renew.addEventListener("click", resetQuiz);
+showScore.addEventListener("click", displayScore);
